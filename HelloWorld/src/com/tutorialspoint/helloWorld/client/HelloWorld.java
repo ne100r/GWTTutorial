b41e5eb934f79b9f5d1d17943a594c022bee8465
ne100r <alexey.ovsienko@gmail.com>
2013-11-20 00:00:27 +0400

    Hello World: small modifications

--- HelloWorld/src/com/tutorialspoint/helloWorld/client/HelloWorld.java
+++ HelloWorld/src/com/tutorialspoint/helloWorld/client/HelloWorld.java
@@ -2,6 +2,8 @@ package com.tutorialspoint.helloWorld.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
@@ -12,6 +14,8 @@ public class HelloWorld implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        Window.alert("Hello, World!");
+        HTML html = new HTML("<p>Welcome to GWT application</p>");
+
+        RootPanel.get("gwtContainer").add(html);
     }
 }
